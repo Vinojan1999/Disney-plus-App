49a5d960e598743d0287872af4c960d1757e0a57
Vinojan Veerapathrathasan <vinojan02abhimanyu@gmail.com>
2022-09-08 13:23:53 +0530

    Add to Firebase & Display Movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,60 @@ import Originals from './Originals';
 import Recommends from './Recommends';
 import Trending from './Trending';
 import Viewers from './Viewers';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import db from '../firebase';
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName } from '../features/user/userSlice';
+import { collection, query, onSnapshot } from 'firebase/firestore';
+
 
 const Home = (props) => {
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    let trending = [];
+
+    useEffect(() => {
+
+        const q = query(collection(db, "movies"));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            // const movies = [];
+            querySnapshot.forEach((doc) => {
+                // movies.push(doc.data().title);
+                switch(doc.data().type) {
+                    case 'recommend' :
+                        recommends.push({id: doc.id, ...doc.data() });
+                        break;
+
+                    case 'new':
+                        newDisneys.push({id: doc.id, ...doc.data() });
+                        break;
+
+                    case 'original':
+                        originals.push({id: doc.id, ...doc.data() });
+                        break;
+
+                    case 'trending':
+                        trending.push({id: doc.id, ...doc.data() });
+                        break;
+                };
+            });
+            // console.log("Current movies: ", movies);
+        });
+
+        dispatch(
+            setMovies({
+                recommend: recommends,
+                newDisney: newDisneys,
+                original: originals,
+                trending: trending,
+            })
+        );
+    }, [userName]);
+
     return (
         <Container>
             <ImgSlider />
